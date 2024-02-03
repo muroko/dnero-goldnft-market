cc73e4a58117ecf9e1371ff870c90dd1537b4b57
Muroko <murokomail@gmail.com>
2024-02-03 09:10:21 -0500

    UPDATE: new contracts in src/utils

--- front-end/src/utils/ipfsStorage.js
+++ front-end/src/utils/ipfsStorage.js
@@ -1,21 +1,30 @@
-import { Web3Storage } from "web3.storage";
+import { NFTStorage } from 'nft.storage';
+//import { filesFromPath } from 'files-from-path'
+//import path from 'path'
 
-const web3storage_key = "";
+const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweDM5NzgwZmJmM0QwNjAxNjliODhiNEEyMGEwNTBiNTc5M2RFRmZmQWUiLCJpc3MiOiJuZnQtc3RvcmFnZSIsImlhdCI6MTcwNjc5NDc1Mjg3MiwibmFtZSI6IkRuZXJvR29sZE5mdC1NYXJrZXRUZXN0In0.OWsUaHOOCuau2icUP680FuzSxFa7FLYrgl8tweCmX-Q';
 
-export const IPFS_GATEWAY = "https://ipfs.io/ipfs/";
-
-function GetAccessToken() {
-  return web3storage_key;
-}
+//export const IPFS_GATEWAY = "https://ipfs.io/ipfs/";
+export const IPFS_GATEWAY = "https://nftstorage.link/ipfs/"; //Gateway from web3.storage
 
 function MakeStorageClient() {
-  return new Web3Storage({ token: GetAccessToken() });
+   return new NFTStorage({ token });
 }
 
+
+  //const storage = new NFTStorage({ token })
+
+
 export const saveContent = async (file) => {
-  console.log("Uploading files to IPFS with web3.storage....");
-  const client = MakeStorageClient();
-  const cid = await client.put([file]);
-  console.log("Stored files with cid:", cid);
+  console.log(`storing file(s)...with nft.storage`);
+  const storage = MakeStorageClient();
+  //const cid = await client.put([file]);
+  //console.log("Stored files with cid:", cid);
+  //
+  //const cid = await storage.storeDirectory(files);
+  const cid = await storage.storeDirectory([file]);
+  console.log("Stored files with cid:", { cid });
   return cid;
+  //const status = await storage.status(cid);
+  //console.log(status);
 };
--- front-end/src/utils/tokens-utils.js
+++ front-end/src/utils/tokens-utils.js
@@ -2,46 +2,46 @@ import { ethers } from "ethers";
 import { mockDAIAddress } from "./contracts-config";
 import images from "../assets/images";
 
-// matic is native coin on the polygon chains
-const MATIC = ethers.constants.AddressZero;
+// dtoken is native coin on the polygon chains
+const DTOKEN = ethers.constants.AddressZero;
 
 const tokens = {
-  "Polygon Mainnet": [
+  "Dnero Mainnet": [
     {
       index: 0,
-      symbol: "MATIC",
-      logo: images.matic,
-      address: MATIC,
+      symbol: "DTOKEN",
+      logo: images.dtoken,
+      address: DTOKEN,
       decimals: 18,
     },
     {
       index: 1,
       symbol: "USDT",
       logo: images.usdt,
-      address: "0xdAC17F958D2ee523a2206206994597C13D831ec7",
+      address: "0xe7EB08Aac8b1f13B19eEF92bb016bf348028607C", //USDT On Dnero Mainnet
       decimals: 6,
     },
     {
       index: 2,
-      symbol: "DAI",
-      logo: images.dai,
-      address: "0x8f3Cf7ad23Cd3CaDbD9735AFf958023239c6A063",
+      symbol: "WDNERO",
+      logo: images.wdnero,
+      address: "0x88e8ec59e782c3e7843ed45e128152707b30d78e", //WDNERO On Dnero Mainnet
       decimals: 18,
     },
     {
       index: 3,
       symbol: "USDC",
       logo: images.usdc,
-      address: "0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174",
+      address: "0x84A84045dCeCa30B82205baD03b62dD214209B6c", //USDC On Dnero Mainnet
       decimals: 6,
     },
   ],
   "Mumbai Testnet": [
     {
       index: 0,
-      symbol: "MATIC",
-      logo: images.matic,
-      address: MATIC,
+      symbol: "DTOKEN",
+      logo: images.dtoken,
+      address: DTOKEN,
       decimals: 18,
     },
     {
@@ -69,9 +69,9 @@ const tokens = {
   Ganache: [
     {
       index: 0,
-      symbol: "MATIC",
-      logo: images.matic,
-      address: MATIC,
+      symbol: "DTOKEN",
+      logo: images.dtoken,
+      address: DTOKEN,
       decimals: 18,
     },
     {
@@ -100,7 +100,7 @@ function formatTokenAmount(network, address, amount) {
 }
 
 export {
-  MATIC,
+  DTOKEN,
   tokens,
   getTokenFromAddress,
   parseTokenAmount,
