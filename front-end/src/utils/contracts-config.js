daa048dfd80a6fba71224b88bbcbc6e8795c4458
Muroko <murokomail@gmail.com>
2024-02-05 08:19:07 -0500

    CHANGE: new contract testing deployed

--- front-end/src/utils/contracts-config.js
+++ front-end/src/utils/contracts-config.js
@@ -1,11 +1,11 @@
 
       // Contracts Deployed And Verified On Dnero Mainnet
       //DGNMMarket
-      export const marketContractAddress = "0x693138B8C2Bf579109E7f174C61d6eDEdFBE1d75"
+      export const marketContractAddress = "0x80f17f2FA3e9E24Aa0772Ac715F547C22ddfeB71"
       //DGNMCollectors
-      export const collectorsContractAddress = "0x859825084e8B703cF090810668088dc2CcF3676C"
+      export const collectorsContractAddress = "0x1Dae253706b7600D8D657b55FB033ae10d018Ad3"
       //DGNMCollection
-      export const nftContractAddress = "0x9294281a610794D27dB09276231Bb67D8D4075Ea"
+      export const nftContractAddress = "0xca38076a23666FcDF544F473e41AA76B981289BF"
       //
       export const mockDAIAddress = "0x8f3Cf7ad23Cd3CaDbD9735AFf958023239c6A063"
       export const ownerAddress = "0x32943D8EDAdD1f202e17115Ef977C9AFcc464c0e"
